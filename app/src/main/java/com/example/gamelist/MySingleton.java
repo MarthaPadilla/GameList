d8de059058d38b72f1f91b496682696a35ea3ec4
MarthaPadilla <66048075+MarthaPadilla@users.noreply.github.com>
2020-07-25 17:39:22 -0600

    En el paquete donde esta el Main creamos una nueva clase que se llame Mysingleton como patron de diseño.

---
+++ app/src/main/java/com/example/gamelist/MySingleton.java
@@ -0,0 +1,50 @@
+package com.example.gamelist;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class MySingleton {
+ private static MySingleton instancia;
+ private RequestQueue requestQueue;
+ private ImageLoader imageLoader;
+ private static Context con;
+
+
+ private MySingleton(Context context){
+     con =context;
+     requestQueue= getRequestQueue();
+
+     imageLoader = new ImageLoader(requestQueue,
+             new ImageLoader.ImageCache() {
+                 @Override
+                 public Bitmap getBitmap(String url) {
+                     return null;
+                 }
+
+                 @Override
+                 public void putBitmap(String url, Bitmap bitmap) {
+
+                 }
+             });
+
+ }
+
+ public static synchronized MySingleton getInstance(Context context){
+     if (instancia == null){
+         instancia= new MySingleton(context);
+     }
+     return instancia;
+ }
+
+ public  RequestQueue getRequestQueue(){
+
+     if (requestQueue==null){
+         requestQueue= Volley.newRequestQueue(con.getApplicationContext());
+     }
+ }
+}
