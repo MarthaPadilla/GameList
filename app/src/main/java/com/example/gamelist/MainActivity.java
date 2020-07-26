ac12a6ec8734cac80fa9897f819c4e3e26f98d4a
MarthaPadilla <66048075+MarthaPadilla@users.noreply.github.com>
2020-07-25 18:39:32 -0600

    Creamos en el main estas variables privadas y luego importamos cada uno

--- app/src/main/java/com/example/gamelist/MainActivity.java
+++ app/src/main/java/com/example/gamelist/MainActivity.java
@@ -1,10 +1,27 @@
 package com.example.gamelist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.gamelist.Adapter.RecyclerViewA;
+import com.example.gamelist.Modelo.Games;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static String URL = "https://rawg-video-games-database.p.rapidapi.com/games";
+    private JsonArrayRequest ArrayRequest;
+    private RequestQueue requestQueue;
+    private ArrayList<Games> listaGame = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TextView txt;
+    private RecyclerViewA myAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
